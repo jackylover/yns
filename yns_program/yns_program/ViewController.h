c5e51856b76b03f0bf9ed024f40787ccfd65f98b
jackylover <jackylovers@126.com>
2017-07-13 09:42:39 +0800

    create yns_program
    创建了益农社项目

---
+++ yns_program/yns_program/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yns_program
+//
+//  Created by xxx on 2017/7/13.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ yns_program/yns_program/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  yns_program
+//
+//  Created by xxx on 2017/7/13.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
